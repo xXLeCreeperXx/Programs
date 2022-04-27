a595a0433f8d770bb411394248af577117ed7350
xXLeCreeperXx <luca-steffens@web.de>
2022-04-27 19:50:23 +0200

    Added all Browsers and chaged the way of how to install programms from PowerShell to CMD

--- Programs.ps1
+++ Programs.ps1
@@ -61,19 +61,19 @@ function Browser-Function {
     switch ($selection)
     {
     '1' {
-        'You chose option #1'
+        Opera-Function
         } 
 
     '2' {
-        'You chose option #2'
+        OperaGX-Function
         } 
 
     '3' {
-        'You chose option #3'
+        Chrome-Function
         }
         
     '4' {
-        'You chose option #4'
+        FireFox-Function
         }    
 
     '5' {
@@ -104,17 +104,166 @@ function Brave-Function {
     {
     '1' {
         Write-Host "Installing Brave Browser"
-        $ResultText.text = "`r`n" +"`r`n" + "Installing Brave... Please Wait" 
-        winget install -e BraveSoftware.BraveBrowser | Out-Host
-        if($?) { Write-Host "Installed Brave Browser" }
-        $ResultText.text = "`r`n" + "Finished Installing Brave" + "`r`n" + "`r`n" + "Ready for Next Task"
+        cmd.exe /c winget install -e BraveSoftware.BraveBrowser
+        Write-Host "Finished Installing Brave Browser" -ForegroundColor red
         Pause
         } 
 
     '2' {
         Write-Host "Uninstalling Brave Browser"
-        winget uninstall -e BraveSoftware.BraveBrowser | Out-Host
-        Write-Host "Finished Uninstalling Brave Browser"
+        cmd.exe /c winget uninstall -e BraveSoftware.BraveBrowser
+        Write-Host "Finished Uninstalling Brave Browser" -ForegroundColor red
+        Pause
+        } 
+    }
+    }
+    until ($selection -eq 'q')
+    
+}
+function FireFox-Text {
+    param (
+        [string]$Title = 'FireFox'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Install"
+    Write-Host "2: Uninstall"
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+function FireFox-Function {
+    do
+    {
+    FireFox-Text
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+        Write-Host "Installing FireFox Browser"
+        cmd.exe /c winget install -e --id Mozilla.Firefox
+        Write-Host "Finished Installing FireFox" -ForegroundColor red
+        Pause
+        } 
+
+    '2' {
+        Write-Host "Uninstalling FireFox Browser"
+        cmd.exe /c winget uninstall -e --id Mozilla.Firefox
+        Write-Host "Finished Uninstalling FireFox" -ForegroundColor red
+        Pause
+        } 
+    }
+    }
+    until ($selection -eq 'q')
+    
+}
+
+function Chrome-Text {
+    param (
+        [string]$Title = 'Chrome'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Install"
+    Write-Host "2: Uninstall"
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+function Chrome-Function {
+    do
+    {
+    Chrome-Text
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+        Write-Host "Installing Chrome Browser"
+        cmd.exe /c winget install -e --id Google.Chrome
+        Write-Host "Finished Installing Chrome" -ForegroundColor red
+        Pause
+        } 
+
+    '2' {
+        Write-Host "Uninstalling Chrome Browser"
+        cmd.exe /c winget uninstall -e --id Google.Chrome
+        Write-Host "Finished Uninstalling Chrome" -ForegroundColor red
+        Pause
+        } 
+    }
+    }
+    until ($selection -eq 'q')
+    
+}
+
+function OperaGX-Text {
+    param (
+        [string]$Title = 'OperaGX'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Install"
+    Write-Host "2: Uninstall"
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+function OperaGX-Function {
+    do
+    {
+    OperaGX-Text
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+        Write-Host "Installing OperaGX Browser"
+        cmd.exe /c winget install -e --id Opera.OperaGX
+        Write-Host "Finished Installing OperaGX" -ForegroundColor red
+        Pause
+        } 
+
+    '2' {
+        Write-Host "Uninstalling OperaGX Browser"
+        cmd.exe /c winget uninstall -e --id Opera.OperaGX
+        Write-Host "Finished Uninstalling OperaGX" -ForegroundColor red
+        Pause
+        } 
+    }
+    }
+    until ($selection -eq 'q')
+    
+}
+
+function Opera-Text {
+    param (
+        [string]$Title = 'Opera'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Install"
+    Write-Host "2: Uninstall"
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+function Opera-Function {
+    do
+    {
+    Opera-Text
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+        Write-Host "Installing Opera Browser"
+        cmd.exe /c winget install -e --id Opera.Opera
+        Write-Host "Finished Installing Opera" -ForegroundColor red
+        Pause
+        } 
+
+    '2' {
+        Write-Host "Uninstalling Opera Browser"
+        cmd.exe /c winget uninstall -e --id Opera.Opera
+        Write-Host "Finished Uninstalling Opera" -ForegroundColor red
         Pause
         } 
     }
